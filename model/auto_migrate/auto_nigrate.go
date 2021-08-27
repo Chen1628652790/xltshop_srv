c40e73a0bdbb4ba9558dc6615750ad230a3a35e5
xiaolatiao <1628652790@qq.com>
2021-08-27 22:32:32 +0800

    feat:自动生成user表

---
+++ model/auto_migrate/auto_nigrate.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/xlt/shop_srv/model"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+func main() {
+	dsn := "root:root@tcp(192.168.199.243:3306)/xltshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // 禁用彩色打印
+		},
+	)
+
+	// 全局模式
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		log.Fatal("gorm.Open failed. err:", err.Error())
+	}
+
+	err = db.AutoMigrate(&model.User{})
+	if err != nil {
+		log.Fatal("db.AutoMigrate failed. err:", err.Error())
+	}
+}
