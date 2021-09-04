006899f439f7dadc00678dced257c1f6b8dda7d1
xiaolatiao <1628652790@qq.com>
2021-09-04 11:29:25 +0800

    feat:品牌创建、删除、更新

--- goods_srv/handler/brands.go
+++ goods_srv/handler/brands.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"context"
 
+	"github.com/golang/protobuf/ptypes/empty"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 
 	"github.com/xlt/shop_srv/goods_srv/global"
@@ -18,7 +21,7 @@ func (handler *GoodsServer) BrandList(ctx context.Context, req *proto.BrandFilte
 	result := global.MySQLConn.Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Find(&brands)
 	if result.Error != nil {
 		zap.S().Errorw("global.MySQLConn.Find failed", "msg", result.Error.Error())
-		return nil, result.Error
+		return nil, status.Errorf(codes.InvalidArgument, "查询品牌列表失败")
 	}
 	rowCount = int(result.RowsAffected)
 
@@ -36,6 +39,48 @@ func (handler *GoodsServer) BrandList(ctx context.Context, req *proto.BrandFilte
 		Total: int32(rowCount),
 		Data:  brandInfoRsp,
 	}, nil
+}
+
+func (handler *GoodsServer) CreateBrand(ctx context.Context, req *proto.BrandRequest) (*proto.BrandInfoResponse, error) {
+	if result := global.MySQLConn.First(&model.Brands{}); result.RowsAffected != 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌已存在")
+	}
+
+	brand := model.Brands{
+		Name: req.Name,
+		Logo: req.Logo,
+	}
+	if result := global.MySQLConn.Create(&brand); result.Error != nil {
+		return nil, status.Errorf(codes.Internal, "创建品牌失败")
+	}
+
+	return &proto.BrandInfoResponse{Id: brand.ID}, nil
+}
+
+func (handler *GoodsServer) DeleteBrand(ctx context.Context, req *proto.BrandRequest) (*empty.Empty, error) {
+	if result := global.MySQLConn.Delete(&model.Brands{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌不存在")
+	}
+	return &empty.Empty{}, nil
+}
+
+func (handler *GoodsServer) UpdateBrand(ctx context.Context, req *proto.BrandRequest) (*empty.Empty, error) {
+	if result := global.MySQLConn.First(&model.Brands{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌不存在")
+	}
+
+	brand := model.Brands{}
+	if req.Name != "" {
+		brand.Name = req.Name
+	}
+	if req.Logo != "" {
+		brand.Logo = req.Logo
+	}
+
+	if result := global.MySQLConn.Save(&brand); result.Error != nil {
+		return nil, status.Errorf(codes.Internal, "更新品牌失败")
+	}
+	return &empty.Empty{}, nil
 }
 
 func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
